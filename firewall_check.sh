b5f1b19834b94825ba1f7088d9e5721424c6ccf7
SRIJESH S <srijesh0304@gmail.com>
2026-09-11 10:01:26 +0530

    Update firewall_check.sh

--- firewall_check.sh
+++ firewall_check.sh
@@ -5,24 +5,24 @@
 # Register Number:
 
 # 1. Check firewalld service status
-
+sudo systemctl status firewalld
 
 # 2. Start firewalld
 # TODO: Complete the command
-
+sudo systemctl start firewalld
 
 # 3. Enable firewalld at boot
 # TODO: Complete the command
-
+sudo systemctl enable firewalld
 
 # 4. Check firewall state
 # TODO: Complete the command
-
+sudo firewall-cmd --state
 
 # 5. Get default firewall zone
 # TODO: Complete the command
-
+sudo firewall-cmd --get-default-zone
 
 # 6. Get active firewall zones
 # TODO: Complete the command
-
+sudo firewall-cmd --get-active-zones
